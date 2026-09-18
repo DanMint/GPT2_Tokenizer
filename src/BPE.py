af8087f78ba69411b26126c38dd83b1fa8dbbf80
Daniel-Strix <dannymints@gmail.com>
2026-09-18 12:52:13 -0400

    Finished BPE implementaion

--- src/BPE.py
+++ src/BPE.py
@@ -35,7 +35,7 @@ class BPE:
     
         return new_tokens
 
-    def train(self, vocab_size: int = 276):
+    def train(self, vocab_size: int = 276) -> None:
         num_merges = vocab_size - 256
 
         for i in range(num_merges):
@@ -48,7 +48,31 @@ class BPE:
         # building out the vocabulary 
         self._vocabulary = {idx: bytes([idx]) for idx in range(256)}
         for (p0, p1), idx in self._merges.items():
-            self._vocabulary[idx] = self._vocabulary[p0] + self._vocabulary[p1] 
+            self._vocabulary[idx] = self._vocabulary[p0] + self._vocabulary[p1]
+
+    def encode(self, text: str) -> list:
+        tokens = list(text.encode("utf-8"))
+
+        while len(tokens) >= 2:
+            stats = self.get_stats(tokens)
+            pair = min(stats, key=lambda p: self._merges.get(p, float("inf")))
+            if pair not in self._merges:
+                break
+            idx = self._merges[pair]
+            tokens = self.merge(tokens, pair, idx)
+
+        return tokens
+
+    def decode(self, tokens: list ) -> str:
+        byte_tokens = []
+
+        for idx in tokens:
+            token_bytes = self._vocabulary[idx]
+            byte_tokens.append(token_bytes)
+    
+        all_bytes = b"".join(byte_tokens)
+        text = all_bytes.decode("utf-8", errors="replace")
+        return text
 
     @property
     def original_text(self) -> str:
@@ -87,7 +111,8 @@ class BPE:
         return len(self._vocabulary)
     
 def main():
-    Test1 = BPE("Ｕｎｉｃｏｄｅ! 🅤🅝🅘🅒🅞🅓🅔‽ 🇺‌🇳‌🇮‌🇨‌🇴‌🇩‌🇪! 😄 The very name strikes fear and awe into the hearts of programmers worldwide. We all know we ought to “support Unicode” in our software (whatever that means—like using wchar_t for all the strings, right?). But Unicode can be abstruse, and diving into the thousand-page Unicode Standard plus its dozens of supplementary annexes, reports, and notes can be more than a little intimidating. I don’t blame programmers for still finding the whole thing mysterious, even 30 years after Unicode’s inception.")
+    training_text = "Ｕｎｉｃｏｄｅ! 🅤🅝🅘🅒🅞🅓🅔‽ 🇺‌🇳‌🇮‌🇨‌🇴‌🇩‌🇪! 😄 The very name strikes fear and awe into the hearts of programmers worldwide. We all know we ought to “support Unicode” in our software (whatever that means—like using wchar_t for all the strings, right?). But Unicode can be abstruse, and diving into the thousand-page Unicode Standard plus its dozens of supplementary annexes, reports, and notes can be more than a little intimidating. I don’t blame programmers for still finding the whole thing mysterious, even 30 years after Unicode’s inception."
+    Test1 = BPE(training_text)
 
     print(Test1.original_text)
     print(Test1.original_tokens)
@@ -101,6 +126,16 @@ def main():
     print(Test1.BPE_tokens_length)
     # print(Test1.vocabulary)
     print(Test1.vocabulary_length)
+
+    ecoding_text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum."
+    BPE_tokens = Test1.encode(ecoding_text)
+
+    print(BPE_tokens)
+
+    print(Test1.decode(BPE_tokens))
+
+
+
 
 if __name__ == "__main__":
     main()
